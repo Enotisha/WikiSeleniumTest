a90cd560784d136ea3301755ad9e800b672625a6
Enotisha <pavlova_pn@skbkontur.ru>
2023-07-19 21:50:04 +0500

    добавили ожидания

--- WikiSeleniumTest/UnitTest1.cs
+++ WikiSeleniumTest/UnitTest1.cs
@@ -1,12 +1,14 @@
 
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace WikiSeleniumTest;
 
 public class WikiSeleniumTest
 {
     private IWebDriver driver;
+    private WebDriverWait wait;
 
     [SetUp]
     public void Setup()
@@ -14,6 +16,8 @@ public class WikiSeleniumTest
         var options = new ChromeOptions();
         options.AddArgument("--start-maximized");
         driver = new ChromeDriver(options);
+        wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5)); //явное ожидание чего угодно.
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5); //неявное ожидание появления элемента на странице.
     }
 
     [Test]
