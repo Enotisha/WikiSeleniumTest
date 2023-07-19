b0fbfd4977c58ff7badc044ce86546996a84e572
Enotisha <pavlova_pn@skbkontur.ru>
2023-07-19 23:47:08 +0500

    добавила локаторы для промо-страницы Диадок

--- WikiSeleniumTest/UnitTest1.cs
+++ WikiSeleniumTest/UnitTest1.cs
@@ -32,6 +32,31 @@ public class WikiSeleniumTest
         Assert.AreEqual("Selenium — Википедия", driver.Title, "Не перешли на страницу про Selenium");
     }
 
+    [Test]
+    public void Locators()
+    {
+        driver.Navigate().GoToUrl("https://konturru-master.ws.testkontur.ru/private/landing?domain=kontur.ru&path=/diadocseleniumref");
+        //Локатор блока “Зарабатывайте на рекомендациях”
+        var orderSection = driver.FindElement(By.Id("order"));
+        //Локатор кнопки “Стать партнером” в блоке в конце страницы
+        var becomePartnerButton = driver.FindElement(By.CssSelector("[value='Стать партнером']"));
+        //Локатор лайтбокса “Заявка на партнерство” (для вызова лайтбокса необходимо нажать кнопку “Стать партнером” в блоке в конце страницы)
+        var becomePartnerLightbox = driver.FindElement(By.CssSelector(".lightbox-window__content"));
+        //Локатор поля “Фамилия”
+        var surnameInput = driver.FindElement(By.CssSelector(".lightbox-window__content input[data-field-role='surname']"));
+        //Локатор поля “Имя”
+        var nameInput = driver.FindElement(By.CssSelector(".lightbox-window__content input[data-field-role='name']"));
+        //Локатор поля “Электронная почта”
+        var emailInput = driver.FindElement(By.CssSelector(".lightbox-window__content input[type='email']"));
+        //Локатор кнопки “Отправить”
+        var submitButton = driver.FindElement(By.CssSelector(".lightbox-window__content button[type='submit']"));
+        //Локатор лайтбокса “Заявка отправлена”
+        var successLightbox = driver.FindElement(By.CssSelector(".form-message.form-message_success"));
+        //Локатор текста в лайтбоксе об успешной отправке 
+        var successMessageText =
+            driver.FindElement(By.CssSelector(".form-message.form-message_success [data-role='success-message-text']"));
+    }
+
     [TearDown]
     public void TearDown()
     {
